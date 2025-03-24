2f26d64567722749e9c91f3b6a3ed6ad1c928d7b
Jasec <jasec_7@hotmail.com>
2025-03-24 00:05:34 -0700

    relocate functions

--- src/index.js
+++ src/index.js
@@ -17,40 +17,6 @@ fetch('http://localhost:3000/movies')
         console.log(allMovies);
     })
 
-movieList.addEventListener('click',(e)=>{
-    if (e.target.classList.contains('add-watchlist-btn')){
-        const movieId = e.target.dataset.id;
-        console.log('clicked movie:', movieId);
-        const alreadyAdded = document.getElementById(`watchlist-item-${movieId}`);
-        if(alreadyAdded){
-            console.log(`Movie ${movieId} already in watchlist.`);
-            return;
-        }
-          
-        const selectedMovie = allMovies.find(movie => movie.id == movieId)
-        const block = document.createElement('div');
-        block.id = `watchlist-item-${movieId}` 
-        block.innerHTML = `
-         <ul> 
-            <h4>${selectedMovie.title}</h4>
-            <img src="${selectedMovie.poster}" width="100">
-            <button class="remove-watchlist-btn">Remove</button>
-            </ul>
-            `
-               console.log(selectedMovie.title);
-
-    document.getElementById('watchlist').appendChild(block)
-    };
-
-    }); 
-    
-    document.getElementById('watchlist').addEventListener('click', (e) => {
-        if(e.target.classList.contains('remove-watchlist-btn')) {
-           e.target.closest('div').remove(); 
-        } 
-
-})
-
     const search = document.getElementById('search-input')
     search.addEventListener('input', (e) =>{
     const searchTerm = e.target.value.toLowerCase();
@@ -58,10 +24,9 @@ movieList.addEventListener('click',(e)=>{
     const filteredMovies = allMovies.filter(movie => 
      movie.title.toLowerCase().includes(searchTerm));
      renderMovies(filteredMovies);
+})
 
-    })
-
-}
+};
 const renderMovies = (movieArray) =>{
     const movieList = document.getElementById('movie-list');
     movieList.innerHTML = "";
@@ -74,6 +39,40 @@ const renderMovies = (movieArray) =>{
         <button class="add-watchlist-btn" data-id="${movie.id}">Add to Watchlist</button>
       </div>
         `
+    })
+
+    movieList.addEventListener('click',(e)=>{
+        if (e.target.classList.contains('add-watchlist-btn')){
+            const movieId = e.target.dataset.id;
+            console.log('clicked movie:', movieId);
+            const alreadyAdded = document.getElementById(`watchlist-item-${movieId}`);
+            if(alreadyAdded){
+                console.log(`Movie ${movieId} already in watchlist.`);
+                return;
+            }
+              
+            const selectedMovie = allMovies.find(movie => movie.id == movieId)
+            const block = document.createElement('div');
+            block.id = `watchlist-item-${movieId}` 
+            block.innerHTML = `
+             <ul> 
+                <h4>${selectedMovie.title}</h4>
+                <img src="${selectedMovie.poster}" width="100">
+                <button class="remove-watchlist-btn">Remove</button>
+                </ul>
+                `
+                   console.log(selectedMovie.title);
+    
+        document.getElementById('watchlist').appendChild(block)
+        };
+    
+        }); 
+        
+        document.getElementById('watchlist').addEventListener('click', (e) => {
+            if(e.target.classList.contains('remove-watchlist-btn')) {
+               e.target.closest('div').remove(); 
+            } 
+    
     })
 }
 
